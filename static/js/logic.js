aa70ce3dff8a9060770fb756a02868e38dd3c078
Mignon Du Plessis <mignonduplessis@Mignons-MacBook-Pro.local>
2020-05-07 10:47:07 -0400

    changed styling

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,6 +5,7 @@ L.tileLayer('https://api.maptiler.com/maps/positron/{z}/{x}/{y}.png?key=G18kR4B5
     {
         attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
     }
+    
 ).addTo(map);
 
 
@@ -24,21 +25,14 @@ d3.json(queryUrl, function (data) {
             fillOpacity: 0.8
         };
     }
+
     function getColor(magnitude) {
-        switch (true) {
-            case magnitude > 5:
-                return "red";
-            case magnitude > 4:
-                return "orange";
-            case magnitude > 3:
-                return "yellow";
-            case magnitude > 2:
-                return "pink";
-            case magnitude > 1:
-                return "green";
-            default:
-                return "white";
-        }
+        return magnitude > 5 ? '#016c59' :
+               magnitude > 4  ? '#1c9099' :
+               magnitude > 3 ? '#67a9cf' :
+               magnitude > 2  ? '#a6bddb' :
+               magnitude > 1   ? '#d0d1e6' :
+                                '#f6eff7';
     }
 
     function getRadius(magnitude) {
@@ -52,7 +46,8 @@ d3.json(queryUrl, function (data) {
 
         style: styleInfo,
         onEachFeature: function (feature, layer) {
-            layer.bindPopup(feature.properties.mag.toString() + feature.properties.place);
+            layer.bindPopup('<br/><b>Magnitude:<b>   ' + feature.properties.mag +
+                '<br/><b>Location:<b>  ' + feature.properties.place);
 
         }
     }).addTo(map)
@@ -62,7 +57,7 @@ d3.json(queryUrl, function (data) {
     legend.onAdd = function (map) {
 
         var div = L.DomUtil.create('div', 'info legend'),
-            color=[0, 1, 2, 3, 4, 5],
+            color = [0, 1, 2, 3, 4, 5],
             labels = [];
 
         // loop through our density intervals and generate a label with a colored square for each interval
